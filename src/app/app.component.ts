fe2e8f2828cbc9d1d1ca391cd9164ecfa6b9fd5b
Antoine CAMILLE <antoine.camille@dotic.fr>
2023-12-07 17:59:06 +0100

    buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,27 @@
-import {AfterViewInit, Component} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import * as leaflet from 'leaflet';
+import { MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {CommonModule} from '@angular/common';
+import {Control} from 'leaflet';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatButtonModule, MatInputModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements AfterViewInit{
   private map!: leaflet.Map;
 
-  public initMap(): void {
+  public ngAfterViewInit(): void {
+    this.initMap();
+    this.initMapControls();
+  }
+
+  private initMap(): void {
     this.map = leaflet.map('map', {
       center: [ 39.8282, -98.5795 ],
       zoom: 3
@@ -28,7 +36,26 @@ export class AppComponent implements AfterViewInit{
     tiles.addTo(this.map);
   }
 
-  ngAfterViewInit(): void {
-    this.initMap();
+  private initMapControls(): void {
+    const h3Control = this.createControl('H3');
+    const s2Control = this.createControl('S2');
+    const geoHashControl = this.createControl('GH');
+
+    new h3Control({ position: 'topright' }).addTo(this.map);
+    new s2Control({ position: 'topright' }).addTo(this.map);
+    new geoHashControl({ position: 'topright' }).addTo(this.map);
+
+  }
+
+  private createControl(name: string): any {
+    return leaflet.Control.extend({
+      onAdd: function () {
+        const element = leaflet.DomUtil.create('button', 'leaflet-bar leaflet-custom-control');
+
+        element.innerHTML = name;
+
+        return element;
+      },
+    });
   }
 }
